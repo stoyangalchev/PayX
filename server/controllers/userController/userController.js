e07ed45d22b5a1e90b6e6943f1b0760f0985f696
stoyangalchev <galchev98@gmail.com>
2025-02-07 21:49:42 +0200

    Add userController functionality for actions

---
+++ server/controllers/userController/userController.js
@@ -0,0 +1,80 @@
+const userModel = require("../../models/userModel/userModel");
+const bcrypt = require("bcrypt");
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    // Check if the user already exists
+    const existingUser = await userModel.getUserByEmail(email);
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already in use" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.createUser({
+      name,
+      email,
+      password: hashedPassword,
+      balance: 0,
+    });
+
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ error: "Failed to register user", error });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.getAllUsers();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await userModel.getUserByEmail(req.params.id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await userModel.updateUser(req.params.id, req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    await userModel.deleteUser(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  registerUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
